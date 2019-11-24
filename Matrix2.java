899a8187b1c107d7c81fec2dfc9d433bb4abfa7e
AHEGAO1707 <egorka.alekseev.2015@mail.ru>
2019-11-24 19:06:59 +0300

    Исправленная реализация класса Matrix2 с return-ами

--- Matrix2.java
+++ Matrix2.java
@@ -20,33 +20,43 @@ public class Matrix2 {
         this.b1 = b1;
         this.b2 = b2;
     }
-    public static void slozheniye(Matrix2 one, Matrix2 two) {
+    public Matrix2(double a11, double a12, double a21, double a22){
+        this.a1 = a11;
+        this.a2 = a12;
+        this.b1 = a21;
+        this.b2 = a22;
+    }
+    public static Matrix2 slozheniye(Matrix2 one, Matrix2 two) {
         System.out.println("Сумма матриц равна => ");
         System.out.println((one.a1 + two.a1) + "     " + (one.a2 + two.a2));
         System.out.println((one.b1 + two.b1) + "     " + (one.b2 + two.b2));
+        return new Matrix2(one.a1 + two.a1, one.a2 + two.a2, one.b1 + two.b1,one.b2 + two.b2);
     }
-    public static void vichitaniye(Matrix2 one, Matrix2 two) {
+    public static Matrix2  vichitaniye(Matrix2 one, Matrix2 two) {
         System.out.println("Разность матриц равна => ");
         System.out.println((one.a1 - two.a1) + "     " + (one.a2 - two.a2));
         System.out.println((one.b1 - two.b1) + "     " + (one.b2 - two.b2));
+        return new Matrix2(one.a1 - two.a1, one.a2 - two.a2, one.b1 - two.b1,one.b2 - two.b2);
     }
-    public static void nachislo(Matrix2 one) {
+    public static  Matrix2 nachislo(Matrix2 one) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число для умножения на матрицу =>");
         double n = in.nextDouble();
         System.out.println("Произведение первой матрицы на число n равно => ");
         System.out.println((one.a1 * n) + "     " + (one.a2 * n));
         System.out.println((one.b1 * n) + "     " + (one.b2 * n));
+        return new Matrix2(one.a1 * n, one.a2 * n, one.b1 * n, one.b2 * n);
     }
-    public static void umnozheniye(Matrix2 one, Matrix2 two) {
+    public static Matrix2 umnozheniye(Matrix2 one, Matrix2 two) {
         System.out.println("Произведение матриц равно => ");
         System.out.println((one.a1*two.a1 + one.a2*two.b1) + "     " + (one.a1*two.a2 + one.a2*two.b2));
         System.out.println((one.b1*two.a1 + one.b2*two.b1) + "     " + (one.b1*two.a2 + one.b2*two.b2));
+        return new Matrix2(one.a1*two.a1 + one.a2*two.b1, one.a1*two.a2 + one.a2*two.b2, one.b1*two.a1 + one.b2*two.b1, one.b1*two.a2 + one.b2*two.b2);
     }
     public static void opredelitel(Matrix2 one) {
         System.out.println("Определитель первой матрицы равен => " + (one.a1*one.b2 - one.a2*one.b1));
     }
-    public static void obratnaya(Matrix2 one) {
+    public static Matrix2 obratnaya(Matrix2 one) {
         double opr = one.a1*one.b2 - one.a2*one.b1;
         if (opr == 0) {
             System.out.println("Данная матрица обратной не имеет.");
@@ -56,6 +66,7 @@ public class Matrix2 {
             System.out.println((1/opr * one.b2) + "     " + (1/opr * -one.a2));
             System.out.println((1/opr * -one.b1) + "     " + (1/opr * -one.a1));
         }
+        return new Matrix2(1/opr * one.b2, 1/opr * -one.a2, 1/opr * -one.b1, 1/opr * -one.a1);
     }
     public static void main(String[] args) {
         Matrix2 one = new Matrix2();
